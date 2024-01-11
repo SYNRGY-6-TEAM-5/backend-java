ad3913f912a82440c20d4177d39f00b7504a46b4
Zeeus <airlanggapermana96@gmail.com>
2024-01-11 14:35:39 +0700

    Update some stuff

--- src/main/java/com/finalproject/Tiket/Pesawat/model/User.java
+++ src/main/java/com/finalproject/Tiket/Pesawat/model/User.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Data
@@ -30,6 +31,9 @@ public class User {
 
     @Column(name = "is_active")
     private Boolean isActive;
+
+    @Column(name = "created_at")
+    private Date createdAt;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "role_id")
