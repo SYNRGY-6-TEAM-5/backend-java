8c6d6733e16b44b41935d8957a9ad8a25932c6e1
Zeeus <airlanggapermana96@gmail.com>
2024-01-16 09:37:49 +0700

    update

--- src/main/java/com/finalproject/Tiket/Pesawat/service/UserService.java
+++ src/main/java/com/finalproject/Tiket/Pesawat/service/UserService.java
@@ -2,8 +2,11 @@ package com.finalproject.Tiket.Pesawat.service;
 
 import com.finalproject.Tiket.Pesawat.dto.user.request.UploadFileRequest;
 import com.finalproject.Tiket.Pesawat.dto.user.response.UploadFileResponse;
+import com.finalproject.Tiket.Pesawat.model.User;
 
 public interface UserService {
 
     UploadFileResponse uploadFile(UploadFileRequest uploadFileRequest);
+
+    Boolean saveNewUserFromOauth2(User user, String imageUrl);
 }
--- src/main/java/com/finalproject/Tiket/Pesawat/service/UserServiceImpl.java
+++ src/main/java/com/finalproject/Tiket/Pesawat/service/UserServiceImpl.java
@@ -2,12 +2,18 @@ package com.finalproject.Tiket.Pesawat.service;
 
 import com.finalproject.Tiket.Pesawat.dto.user.request.UploadFileRequest;
 import com.finalproject.Tiket.Pesawat.dto.user.response.UploadFileResponse;
+import com.finalproject.Tiket.Pesawat.exception.EmailAlreadyRegisteredHandling;
 import com.finalproject.Tiket.Pesawat.exception.ExceptionHandling;
 import com.finalproject.Tiket.Pesawat.exception.UnauthorizedHandling;
+import com.finalproject.Tiket.Pesawat.model.EnumRole;
 import com.finalproject.Tiket.Pesawat.model.Images;
+import com.finalproject.Tiket.Pesawat.model.Role;
 import com.finalproject.Tiket.Pesawat.model.User;
+import com.finalproject.Tiket.Pesawat.repository.RoleRepository;
 import com.finalproject.Tiket.Pesawat.repository.UserRepository;
 import com.finalproject.Tiket.Pesawat.security.service.UserDetailsImpl;
+import com.finalproject.Tiket.Pesawat.utils.Utils;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,12 +22,16 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
+@Log4j2
 public class UserServiceImpl implements UserService {
     @Autowired
     private CloudinaryService cloudinaryService;
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Override
     public UploadFileResponse uploadFile(UploadFileRequest uploadFileRequest) {
@@ -35,6 +45,8 @@ public class UserServiceImpl implements UserService {
                 if (userOptional.isEmpty()) {
                     throw new UnauthorizedHandling("User Not Found");
                 }
+
+                log.info("user a " + ((UserDetailsImpl) principal).getUsername());
                 User user = userOptional.get();
                 if (uploadFileRequest.getName().isEmpty()) {
                     return null;
@@ -67,5 +79,31 @@ public class UserServiceImpl implements UserService {
             throw new ExceptionHandling(e.getMessage());
         }
         throw new UnauthorizedHandling("Unknown principal type");
+    }
+
+    @Override
+    public Boolean saveNewUserFromOauth2(User user, String imageUrl) {
+        try {
+            Optional<User> userOptional = userRepository.findByEmailAddress(user.getEmailAddress());
+            if (userOptional.isPresent()){
+                throw new EmailAlreadyRegisteredHandling();
+            }
+//            Optional<Role> optionalUserRole = roleRepository.findByRoleName(EnumRole.USER);
+            Images image = Images.builder()
+                    .name(user.getFullname())
+                    .url(imageUrl)
+                    .build();
+
+//            user.setRole(); todo
+            user.setImages(image);
+            user.setFullname(user.getFullname());
+            user.setCreatedAt(Utils.getCurrentDateTimeAsDate());
+            userRepository.save(user);
+            log.info("sukses create user");
+        } catch (Exception e) {
+            log.error("Failed Save new Oauth2 User " + e.getMessage());
+            return false;
+        }
+        return true;
     }
 }
